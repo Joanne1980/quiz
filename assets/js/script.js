e867999d0413d4d8910469e974887e7683ef837d
Joanne Clarke <joanne.clarke23@hotmail.com>
2023-01-13 12:20:05 +0000

    set up pseudocode ready to start project

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+/*Create a code quiz that contains the following requirements:
+
+* A start button that when clicked a timer starts and the first question appears.
+ 
+  * Questions contain buttons for each answer.
+  * 
+  * When answer is clicked, the next question appears
+  * 
+  * If the answer clicked was incorrect then subtract time from the clock
+
+* The quiz should end when all questions are answered or the timer reaches 0.
+
+  * When the game ends, it should display their score and give the user the ability 
+    to save their initials and their score*/
+    
+    //create a list of questions
+    
+    //click start button, timer to start and first question to appear. with list of 
+    //multiply chose answers.
+
+    //click button to relevant answer
+
+    // when answer is clicked confirm if correct if incorrect deduct time from counter
+
+    //run through all questions unless time runs out. 
+
+    //when game ends give user their score and let them record their initials and score 
+
+
+    
+
