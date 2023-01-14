f9ea766397ce305d505286ac650285498e264bf4
Joanne Clarke <joanne.clarke23@hotmail.com>
2023-01-14 10:55:21 +0000

    questions added to questions.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -26,6 +26,87 @@
 
     //*when game ends give user their score and let them record their initials and score 
 
+   var score = 0;
+   var currentQuestion = 0;
+   var counter;
+   var timer;
 
-    
+   //prepare all selector that we might need to point to the html element
+   var startButton = document.querySelector('#start');
+  //startScreenElement
+  //questionsContainer
+  //questionsTitle
+  //choicesContainer
+  //timerContainer
+  //....
 
+   function populateQuestion(question) {
+    var question = question.title;
+    var choices = question.choices;
+   
+    choicesContainer.innerHTML = '';
+  //questionTitle.textContent = questions;
+    var choiceList = document.createElement('ul');
+    for(let i = 0; i < choices.length; i++){
+    var choice = document.createElement('li');
+    choice.textContent = choices[i];
+    choiceList.appendChild(choice);
+  }
+  //choicesContainer.appendChild(choicesList)
+  }
+
+  function endGame() {
+  //when the game ends, it should display their score and give the user the 
+  //ability to save their initials and their score
+
+  //h ide questions container
+  //show endscreen container
+  //assign score to finalscore container
+  //reset the timer clearInterval(timer);
+  }
+
+   function nextQuestion() {
+    currentQuestion++;
+    if (currentQuestion < questions.length) {
+      populateQuestion(questions[currentQuestion]);
+    } else {
+      endGame();
+    }
+
+  }
+
+  startButton.addEventListener('click', function() {
+  startScreenElement.setAttribute('class', 'hide');
+  questionsContainer.setAttribute('class', 'visable');
+
+  // show the first question
+  currentQuestion = 0;
+  populateQuestion(question[currentQuestion]);
+
+  counter = 100;
+  timer = setInterval(function() {
+    counter--;
+    // set timerContainer text to counter
+    if (counter <= 0) {
+      //endGame()
+      clearInterval(timer);
+    }
+  }, 1000);
+  });
+
+function saveHighscore(initial) {
+  //get the current highscores value from localstorage
+  //json parse current highscores from localstorage, this will be an array of object
+  //push initial + score to the array
+  //order the array from highest score to lowest
+  //json stringify then save back to localstorage
+ }
+  //another click event listener for choices
+  //check answer
+  //if correct, add 1 to score, call nextQuestion()
+  //if wrong, remove 10 seconds from the inverval, call nextQuestion()
+
+  //click event listener to submit button
+  //var initial = initialInput.value.trim()
+  //saveHighscore(initial)
+  //redirect to highscore page
---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+var questions =[
+    {
+        title: "How do you write 'Hello World'in an alert box?",
+        choices: ["msg('Hello World')", "msgBox('Hello World')", "alertBox('Hello World')", "alert('Hello World')"],
+        answer: "alert('Hello World')"
+    },
+    {
+        title: "How to empty an array in JavaScript?",
+        choices: ["arrayList[]", "arrayList(0)", "arrayList.Length=0", "arrayList.len(0)"],
+        answer: "arrayList.length=0"
+    },
+    {
+        title: "What function to add an element at the beginning of an arrayand one at the end?",
+        choices: ["push,unshift", "unshift,push", "first,push", "unshift,last"],
+        answer: "unshift,push"
+    },
+    {
+        title: "What will this output? var = a =[1,2,3]; console.log(a[6])",
+        choices: ["undefined", "0", "prints nothing", "syntax error"],
+        answer: "undefined"
+    },
+    {
+       title: "What would the following code return? console.log(typeof typeof 1)",
+       choices: ["string", "number", "syntax error", "undefined"],
+       answer:  "String"
+    },
+    {
+        title: "Which software company developed JavaScript?",
+        choices: ["Mozilla", "Netscape", "Sun Microsystems", "Oracle"],
+        answer: "Netscape"
+    },
+    {
+        title: "What would be the result of 3+2+'7'?",
+        choices: ["327", "12", "14", "57"],
+        answer: "57"
+    },
+    {
+        title: "Look at the following selector: $('div'). What does it select?",
+        choices: ["All div elements", "Current div element", "First div element", "Last div element"],
+        answer: "All div elements"
+    },
+    {
+        title: "How can a value be appended to an array?",
+        choices: ["arr(length).value", "arr[]=add(value)", "arr[arr.length]=value", "None of the these"],
+        answer: "arr[arr.length]=value"
+    },
+    {
+        title: "What will the code below output to the console? console.log(1 + +'2' +'2')",
+        choices: ["32", "122", "13", "14"],
+        answer: "32"
+    },
+]
