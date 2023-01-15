1a8bfd44fa6bb0d1a943cca3dbc4f979361adae1
Joanne Clarke <joanne.clarke23@hotmail.com>
2023-01-15 09:09:05 +0000

    Started js process again as got confused with code. Start button now working need to load first question

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,4 @@
-/*Create a code quiz that contains the following requirements:
+ /*Create a code quiz that contains the following requirements:
 
 * A start button that when clicked a timer starts and the first question appears.
  
@@ -26,87 +26,53 @@
 
     //*when game ends give user their score and let them record their initials and score 
 
-   var score = 0;
-   var currentQuestion = 0;
-   var counter;
-   var timer;
-
-   //prepare all selector that we might need to point to the html element
+    //Start button
    var startButton = document.querySelector('#start');
-  //startScreenElement
-  //questionsContainer
-  //questionsTitle
-  //choicesContainer
-  //timerContainer
-  //....
 
+    //Populate questions
    function populateQuestion(question) {
     var question = question.title;
     var choices = question.choices;
-   
+   }
+    // link questions to html
+    var choicesContainer = document.querySelector('#choices')
+    var questionTitle = ('question');
     choicesContainer.innerHTML = '';
-  //questionTitle.textContent = questions;
-    var choiceList = document.createElement('ul');
-    for(let i = 0; i < choices.length; i++){
-    var choice = document.createElement('li');
-    choice.textContent = choices[i];
-    choiceList.appendChild(choice);
-  }
-  //choicesContainer.appendChild(choicesList)
-  }
-
-  function endGame() {
-  //when the game ends, it should display their score and give the user the 
-  //ability to save their initials and their score
-
-  //h ide questions container
-  //show endscreen container
-  //assign score to finalscore container
-  //reset the timer clearInterval(timer);
-  }
-
-   function nextQuestion() {
-    currentQuestion++;
-    if (currentQuestion < questions.length) {
-      populateQuestion(questions[currentQuestion]);
-    } else {
-      endGame();
+    questionTitle.textContent = questions;
+    var choicesList = document.createElement('ul');
+    for (let i = 0; i < choices.length; i++) {
+        var choice = document.createElement('li');
+        choice.textContent = choices[i];
+        choicesList.appendChild(choice);
     }
 
-  }
+    //Start button is clicked
+    var startScreenElement = document.querySelector('#start-screen');
+    var questionsContainer = document.querySelector('#questions');
+    startButton.addEventListener('click', function() {
+    startScreenElement.setAttribute('class', 'hide');
+    questionsContainer.setAttribute('class', 'visible');
+    })
+    // show the first question
+    currentQuestion = 0;
+    populateQuestion(questions[currentQuestion]);
 
-  startButton.addEventListener('click', function() {
-  startScreenElement.setAttribute('class', 'hide');
-  questionsContainer.setAttribute('class', 'visable');
+    counter = 100;
+    timer = setInterval(function() {
+        counter--;
+        // set timerContainer text to counter
+        if (counter <= 0) {
+            // endGame()
+            clearInterval(timer);
+        }
+    }, 1000);
 
-  // show the first question
-  currentQuestion = 0;
-  populateQuestion(question[currentQuestion]);
-
-  counter = 100;
-  timer = setInterval(function() {
-    counter--;
-    // set timerContainer text to counter
-    if (counter <= 0) {
-      //endGame()
-      clearInterval(timer);
-    }
-  }, 1000);
-  });
-
-function saveHighscore(initial) {
-  //get the current highscores value from localstorage
-  //json parse current highscores from localstorage, this will be an array of object
-  //push initial + score to the array
-  //order the array from highest score to lowest
-  //json stringify then save back to localstorage
- }
-  //another click event listener for choices
-  //check answer
-  //if correct, add 1 to score, call nextQuestion()
-  //if wrong, remove 10 seconds from the inverval, call nextQuestion()
-
-  //click event listener to submit button
-  //var initial = initialInput.value.trim()
-  //saveHighscore(initial)
-  //redirect to highscore page
+      //next question
+    function nextQuestion() {
+      currentQuestion++;
+      if (currentQuestion < questions.length) {
+          populateQuestion(questions[currentQuestion]);
+      } else {
+          endGame();
+      }
+    };
